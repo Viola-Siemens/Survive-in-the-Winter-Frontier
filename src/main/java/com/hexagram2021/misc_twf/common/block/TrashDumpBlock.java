8062145f0124836df1dd5cd46f3b47ed21f8c82c
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2026-08-12 22:40:45 +0800

    fix: 垃圾堆碰撞箱调整

--- src/main/java/com/hexagram2021/misc_twf/common/block/TrashDumpBlock.java
+++ src/main/java/com/hexagram2021/misc_twf/common/block/TrashDumpBlock.java
@@ -1,12 +1,16 @@
 package com.hexagram2021.misc_twf.common.block;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 /**
  * 垃圾堆方块喵~
@@ -16,9 +20,16 @@ import net.minecraft.world.level.block.state.StateDefinition;
 public class TrashDumpBlock extends HorizontalDirectionalBlock {
 	public static final MapCodec<TrashDumpBlock> CODEC = simpleCodec(TrashDumpBlock::new);
 
+	protected static final VoxelShape SHAPE = Block.box(1, 0, 1, 15, 8, 15);
+
 	public TrashDumpBlock(Properties props) {
 		super(props);
 		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState blockState, BlockGetter level, BlockPos blockPos, CollisionContext context) {
+		return SHAPE;
 	}
 
 	@Override
